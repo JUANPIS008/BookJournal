88356c353d9336f32ec5886386fac15fc2e1a0b5
Maria Paula <mpriverosb@libertadores.edu.co>
2026-04-16 12:34:28 -0500

    cod: creacion de la rama front-end, login.html, login.css y login.js #25

---
+++ front-end/java-script/login.js
@@ -0,0 +1,49 @@
+
+
+function irARegistro() { 
+    window.location.href = "registro.html"; 
+}
+
+function irHome() { 
+    window.location.href = "lectura_actual.html"; 
+}
+
+async function login() {
+    const correo = document.getElementById('user').value;
+    const password = document.getElementById('pass').value;
+
+    if (!correo || !password) {
+        alert("Completa todos los campos");
+        return;
+    }
+
+    try {
+        const respuesta = await fetch(`${API_URL}/login`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ correo, password })
+        });
+
+        if (!respuesta.ok) {
+            alert("Credenciales incorrectas");
+            return;
+        }
+
+        const usuario = await respuesta.json();
+
+        if (usuario) {
+            localStorage.setItem("usuarioLogueado", JSON.stringify(usuario));
+
+            alert("Login exitoso 🎉");
+            irHome();
+        } else {
+            alert("Credenciales incorrectas");
+        }
+
+    } catch (error) {
+        console.error("Error:", error);
+        alert("Error conectando con el servidor");
+    }
+}
