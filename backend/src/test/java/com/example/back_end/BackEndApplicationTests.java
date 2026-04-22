e95dd11d731861bc787c8fa90c41642346cf316f
Maria Paula <mpriverosb@libertadores.edu.co>
2026-04-22 14:36:14 -0500

    desarrollo pruebas de integracion-frontend #29

--- backend/src/test/java/com/example/back_end/BackEndApplicationTests.java
+++ backend/src/test/java/com/example/back_end/BackEndApplicationTests.java
@@ -1,13 +1,37 @@
 package com.example.back_end;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
 
 // @SpringBootTest
 class BackEndApplicationTests {
 
-	// @Test
-	void contextLoads() {
-	}
+	    @Autowired
+    private MockMvc mockMvc;
 
+    @Test
+    public void testGuardarLibroIntegracion() throws Exception {
+        // Simulamos los datos de un libro para la prueba
+        String libroJson = "{\"titulo\":\"La casa de los espiritus\",\"autor\":\"Isabel Allende\",\"genero\":\"Ficcion\",\"calificacion\":5}";
+
+        // Probamos el endpoint de tu backend
+        mockMvc.perform(post("/api/libros")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(libroJson))
+                .andExpect(status().isCreated());
+    }
+	
 }
